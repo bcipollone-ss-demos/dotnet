39ddc7065f50ba1356475d9b0c1fea2fba0bdc86
Brian Cipollone <40029762+brian-cipollone-sonarsource@users.noreply.github.com>
2023-06-27 13:37:19 -0500

    Delete scanner-csharp.sh

--- scanner-csharp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dotnet restore
-dotnet sonarscanner begin /k:"csharp-dotnetcore-sonar" /n:"csharp-dotnetcore-sonar" /d:sonar.verbose=true /d:sonar.login="input your token here"
-dotnet build 
-dotnet sonarscanner end /d:sonar.login="input your token here"
